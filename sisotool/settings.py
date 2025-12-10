8b8302a66f0823e650ad87375d296f0780806f75
Briyith-07 <briyith.jimenez2001@gmail.com>
2025-12-10 15:05:06 -0500

    Update settings.py

--- sisotool/settings.py
+++ sisotool/settings.py
@@ -123,3 +123,5 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('1', 'true',
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+
+ALLOWED_HOSTS = ['web-production-f158c.up.railway.app']
